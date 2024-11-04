c2cd3a78360984d393d16a3616e6e654e9fbd625
Jhon01-varas <jhonmaycolvaraspimentel@gmail.com>
2024-11-04 11:01:00 -0500

    actualizacion 7.5

--- script/script.js
+++ script/script.js
@@ -8,3 +8,18 @@ window.addEventListener("scroll", function() {
         mainImage.classList.remove("hidden");
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const section = document.querySelector('.mission-vision-section');
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                section.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once visible
+            }
+        });
+    });
+
+    observer.observe(section);
+});
